697deb036f325f1fc6892ccb05552851601cfa05
az-saca <churunain.ra@unipdu.ac.id>
2023-12-28 23:17:03 +0700

    task commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export const getMovieList = async() => {
+    const movie = await axios.get(
+        `${process.env.REACT_APP_BASEURL}/movie/popular?page=1&api_key=${process.env.REACT_APP_APIKEY}`
+    )
+    return movie.data.results
+}
+
+export const searchMovie = async(q) => {
+    const search = await axios.get(
+        `${process.env.REACT_APP_BASEURL}/search/movie?page=1&query=${q}&api_key=${process.env.REACT_APP_APIKEY}`
+    )
+    return search.data
+}
